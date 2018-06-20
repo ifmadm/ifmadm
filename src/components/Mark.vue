d0dde890c53395883f86fa2a66c5480ec7a68189
Chengrui Fu <chengrui.fu@duke.edu>
2018-06-19 20:25:39 -0700

    changes

--- src/components/Mark.vue
+++ src/components/Mark.vue
@@ -2,7 +2,7 @@
   <div id="marketing-page">
         <nav>
           <div class="nav-wrapper">
-            <a href="/" class="brand-logo">
+            <a href="/">
               <img src="../assets/img/Logo_w.png" alt="Fashion X Project">
             </a>
           </div>
@@ -15,11 +15,13 @@
                   Share your looks &amp; earn money.
                 </span>
                 <br/>
-                <b>Transform your shopping experience.</b>
               </h4>
               <div class="hero-center">
                   <h4>Leading the ne<span>x</span>t generation of inclusive fashion</h4>
               </div>
+              <h4>
+                <b>Transform your shopping experience.</b>
+              </h4>
           </div>
         </section>
         <section id="signup">
@@ -29,24 +31,41 @@
                 <ul class="collection info-sec">
                   <li class="collection-item avatar">
                     <span class="circle btn-floating btn-large deep-orange lighten-3 z-depth-3">1</span>
-                    <h5>Sign up</h5>
+                    <h5><a href="#signform">Sign up</a></h5>
                   </li>
                   <li class="collection-item avatar">
                     <span class="circle btn-floating btn-large deep-orange lighten-3 z-depth-3">2</span>
-                    <h5>See a curated feed from people with a body similar to yours, wearing cool new clothes from top retailers</h5>
+                    <h5>Receive our newsletter with a curated feed from people with a body similar to yours, wearing cool new clothes from top retailers</h5>
                   </li>
                   <li class="collection-item avatar">
                     <span class="circle btn-floating btn-large deep-orange lighten-3 z-depth-3">3</span>
-                    <h5>Shop clothes, directly from the pics</h5>
+                    <h5>Shop clothes, directly from the pic</h5>
                   </li>
                   <li class="collection-item avatar">
                     <span class="circle btn-floating btn-large deep-orange lighten-3 z-depth-3">4</span>
-                    <h5>Upload your own pictures trying on new clothes and earn money</h5>
+                    <h5>
+                      <a href="#">Upload your own pictures</a>
+                      trying on new clothes and earn money</h5>
                   </li>
                 </ul>
             </div>
-            <div class="col m12 l6 signform">
-                  <form class="z-depth-5">
+            <div id="signform" class="col m12 l6 signform">
+                <div class="row">
+                  <form action="" class="container z-depth-5">
+                    <img src="../assets/img/Logo_w.png">
+                    <p style="margin-bottom:2.5rem">Subscribe to our mailing list to receive all our cool Instagram outfits and shop them!</p>
+                    <div class="inner">
+                      <div class="input-field" >
+                        <input placeholder="my@email.com" id="email" type="email" class="validate">
+                        <label class="active" for="email">Email</label>
+                      </div>
+                      <button class="btn waves-effect waves-light" type="submit" name="action" @click="" style="margin-bottom:1rem;max-width:260px">Sign Up</button>
+                    </div>
+                    <p class="grey-text">Beta access exclusive to first 1000 users who sign up.<br/>Fashion X Project for men launching December 2018.</p>
+                  </form>
+                </div>
+              
+                  <!-- <form class="z-depth-5">
                     <div class="signup-call">
                         <span>Sign Up Now</span>
                     </div>
@@ -65,7 +84,7 @@
                         <div class="input-field">
                           <input id="instagram" type="text" class="validate">
                           <label for="instagram">Instagram Handle (ID)</label>
-                          <!-- <span class="helper-text" data-error="wrong" data-success="right">Instagram Handle (ID)</span> -->
+                          < !- - <span class="helper-text" data-error="wrong" data-success="right">Instagram Handle (ID)</span> - ->
                         </div>
                       </div>
                     </div>
@@ -126,15 +145,15 @@
                       <label>Beta access exclusive to first 1000 users who sign up.</label>
                       <br>
                       <label>fashionxproject for men launching December 2018.</label>
-                    </p>
-                  </form>
+                    </p> 
+                  </form>-->
             </div>
           </div>
         </section>
         <section class="container-extended deep-orange lighten-3 info">
           <div class="row">
             <div class="col m12 l6">
-              <img class="materialboxed" src="../assets/img/iPhone_X-2.png" style="width:400;margin: 0 auto;padding:2rem;">
+              <img class="materialboxed" src="../assets/img/iPhone_X.png">
             </div>
             <div class="col m12 l6 info-int">
               <h3 style="color:#fff">About</h3>
@@ -175,7 +194,7 @@
                 </div>                
             </div>
           </div>
-          <a class="btn-flat" href="#signup">Sign Up</a>
+          <a class="btn-flat" href="#signform">Sign Up</a>
         </section>
         <section class="teal lighten-2 trenders">
           <div class="row">
@@ -187,7 +206,7 @@
                 <img src="../assets/img/contract.png">
               </div>
               <div class="card-content teal lighten-2 text-center">
-                  <p class="flow-text"><a href="#signup">Sign up</a></p>
+                  <p class="flow-text"><a href="#signform">Sign up</a></p>
               </div>
             </div>
           </div>
@@ -222,7 +241,7 @@
               <div class="col l6 m10 s12 z-depth-5 content">
                 <p class="flow-text">Drive more traffic to the content you work hard to create. Share your content with us and we will link it to your blog</p>
                 <p class="flow-text">Care about inclusive fashion? Our shoppers also do. Now they can buy from your content based on their style preferences, body characteristics or personality.</p>
-                <p class="flow-text"><a href="#signup">Sign up</a> &amp; add the link to your blog and <a href="mailto:contact@fashionxproject.com?subject=Influencer "> email us</a> any extra content to share</p>
+                <p class="flow-text"><a href="#signform">Sign up</a> &amp; add the link to your blog and <a href="mailto:contact@fashionxproject.com?subject=Influencer "> email us</a> any extra content to share</p>
                 <p class="flow-text">Share <a href="https://www.instagram.com/fashionXproject" target="_blank">@fashionXproject</a> with your followers and use <a href="https://www.instagram.com/explore/tags/fashionxproject/" target="_blank">#fashionXproject</a> in your content to empower the movement!</p>
               </div>
             </div>
@@ -287,7 +306,6 @@
             </div>
           </div>
         </footer>
-
   </div>
 </template>
 
@@ -356,41 +374,31 @@ export default {
 </script>
 
 <style>
-
 nav {
   background-color: transparent;
   -webkit-box-shadown: none;
   box-shadow: none;
 }
-nav .brand-logo {
-  color: #001200;
-  font-weight: 600;
-}
 .brand-logo {
   margin-left: 3rem;
 }
 .brand-logo img {
-  width: 300px;
+  width: 100%;
   padding-top: 1rem;
 }
-
 /* Hero */
-
 #hero {
   text-align: center;
-  margin-top: 3rem;
+  margin-top: 6rem;
 }
 #hero h4 {
   padding: 0.5rem 0 0.8rem;
   letter-spacing: 2.5px;
   line-height: 1.5;
 }
-
 .hero-center {
-  background: rgba(244, 143, 177, 0.6) url(../assets/img/hero-img-2.png) center/cover no-repeat;
-}
-.hero-center {
-  padding: 15rem 0;
+  background: url(../assets/img/hero-img-2.png) center/cover no-repeat;
+  padding: 12rem 0;
   color: #001200;
  
 }
@@ -403,13 +411,13 @@ nav .brand-logo {
 .hero-center span {
   color:pink;
 }
-
 /* Sign Up */
 #signup {
   padding: 5rem;
   text-align: center;
-  background-color: #f8f8f8;
+
 }
+
 .signup-call {
   padding: 0 0 3rem;
   text-align: center;
@@ -424,15 +432,27 @@ nav .brand-logo {
   color: white;
   border-bottom: 5px solid #024602;
 }
-#signup .container {
-  padding:3rem 5rem 5rem;
+.signform .container {
+  padding:4rem 2.5rem 5rem;
+}
+@media only screen and (min-width: 993px) {
+  .signform .container {
+    width: 75%;
+  }
+}
+.signform .inner {
+  margin-top: 4rem;
+  text-align: left;
+}
+.signform img {
+  max-width: 256px;
+  margin-bottom: 2rem;
 }
 #signup form {
-  text-align: left;
-  max-width: 700px;
-  margin: 0 auto;
-  padding: 5rem;
   background-color: #ffffff;
+}
+#signup p {
+  text-align: left;
 }
 #signup .btn {
   font-size: 18px;
@@ -473,7 +493,11 @@ nav .brand-logo {
 #signup [type="checkbox"] + span:not(.lever) {
   padding-left: 24px;
 }
-
+#signup .info-sec h5 a {
+  text-decoration: underline;
+  color: #00000085;
+  font-weight: 700;
+}
 /* Info */
 .info {
   text-align: left;
@@ -502,18 +526,17 @@ nav .brand-logo {
 .info-int {
   padding: 5rem;
 }
-
-
-
+.info img {
+  width:350px;
+  margin: 0 auto;
+  padding:2rem;
+}
 /* Shopper */
-
 #shopper {
   padding: 10rem;
   background: url(../assets/img/img-grid.png) right no-repeat;
   background-size:contain; 
 }
-
-
 #shopper .row .col.s1 {
   width: 5.333333%;
 }
@@ -537,24 +560,18 @@ nav .brand-logo {
 #shopper a:hover {
   text-decoration: none;
 }
-
-
-
 /* Trenders */
 .trenders {
   padding: 5rem;
-  /* height: -webkit-fill-available; */
   margin-bottom: 0;
 }
 .trenders .title {
   color: #001200;
   padding-left: 5rem;
 }
-
 .trenders .card {
   background-color: transparent;
   box-shadow: none;
-  padding: 1rem 4rem;
 }
 .trenders .card-image img {
   width: 12rem;
@@ -583,7 +600,6 @@ nav .brand-logo {
 .trenders p span {
   font-weight: 700;
 }
-
 /* Influencers */
 #influencers {
   padding: 3rem 0 12rem;
@@ -609,7 +625,6 @@ nav .brand-logo {
 #influencers .content a:active {
   color: #874b5f;
 }
-
 /* Retailer */
 #retailer {
   padding: 5rem 0;
@@ -644,16 +659,13 @@ nav .brand-logo {
   height: 60px;
   padding: 12px;
 }
-
 /* Footer */
-
 footer img {
   margin-right: 1rem;
 }
 footer a {
   color: #001200;
 }
-
 /* General */
 h1 {
   font-size: 4rem;
@@ -668,7 +680,6 @@ h4 {
 .img-full {
   width: 100%;
 }
-
 /* Media Query */
 @media only screen and (max-width: 1063px) {
   #signup form {
@@ -700,6 +711,12 @@ h4 {
   }
 }
 @media only screen and (max-width:768px) {
+    .nav-wrapper{
+        text-align: center;
+    }
+    .nav-wrapper img{
+        max-width: 320px;
+    }
   #hero h4,
   .trenders h4,
   #influencers h4,
@@ -712,14 +729,17 @@ h4 {
   .signup-sub {
     padding: 5rem;
   }
+  .signform img {
+    width: 100%;
+  }
   .info {
-    padding: 5rem;
+    padding: 5rem 3rem;
   }
   .info-sec {
     padding-left: 0 !important;
   }
   #shopper {
-    padding: 4rem;
+    padding: 4rem 2rem;
     background: #ffffff;
   }
   #shopper img.circle {
@@ -729,7 +749,10 @@ h4 {
   #shopper h1,
   #influencers h1,
   #retailer h1 {
-    font-size: 4rem !important;
+    font-size: 2.5rem !important;
+  }
+  #retailer .btn-act {
+    width: 100%;
   }
   .trenders {
     padding: 2rem;
@@ -738,19 +761,29 @@ h4 {
     padding-left: 0;
   }
   .img-full {
-    width: 346px;
+    width: 100%;
+  }
+  #influencers .content {
+    padding: 5rem 2rem;
   }
   .deck {
     margin-bottom: 4rem;
   }
+  .info img {
+  width:100%;
+  padding:1rem;
+}
 }
 @media only screen and (max-width:500px) {
   #signup {
-    padding: 2rem;
+    padding: 3rem 1rem;
   }
   .info-int {
     padding: 5rem 1rem;
   }
+  #signup form {
+    padding: 3rem 1.5rem;
+}
 }
     
 @media only screen and (max-width:1200px) {
